3b3cd4363f3ad60914112b4f03fff6d884ebfd0d
v_xyxinyan <v_xyxinyan@tencent.com>
2020-07-18 19:49:14 +0800

    delete 101

--- 101/Lua/GamePlay/Controller/ItemController.lua
+++
@@ -1,7 +0,0 @@
--- 根据导入的Item数据进行一个重新分类
-
-if ItemController==nil then 
-    ItemController={}
-end
-function ItemController:init()
-end
--- 101/Lua/GamePlay/EntityClass/EntityManager.lua
+++
@@ -1,5 +0,0 @@
--- 用于管理游戏中的实体，获取他们的坐标
-
-if EntityManager==nil then 
-    EntityManager={}
-end
